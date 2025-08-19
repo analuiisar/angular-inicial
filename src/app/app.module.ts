5e15b0154de29968ce5685d5a06f2a1b8d511c9f
Érick Souza de Melo <analuiisar@gmail.com>
2025-08-19 19:05:14 -0300

    criando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicoComponent } from './servico/servico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicoComponent
   ],
   imports: [
     BrowserModule,
